75079630fdc2b0f1b553d0bbfc7f06afb6566e6c
dabhi prakash <dabhiprakash32244@gmail.com>
2025-11-07 23:04:06 +0530

    fixed

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'dataglimpse/lib',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2ef603910e92446a1f82098b988fb63672b1d908',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'dataglimpse/lib' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2ef603910e92446a1f82098b988fb63672b1d908',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
